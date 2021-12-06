72fd893d0af3d684b8c9a70ee69ed6f06005e2bd
Amit kumar <40890972+amitkumar0000@users.noreply.github.com>
2021-12-06 07:11:36 +0530

    Update Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,33 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+     fun part1(input: List<String>): Int {
+        var prev = -1
+        var count = 0
+        for(l in input.indices) {
+            if(prev == -1) {
+                prev = input[l].toInt()
+                continue
+            }
+            if(input[l].toInt() > prev) {
+                count++
+            }
+            prev = input[l].toInt()
+
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+         if(input.size < 3)
+            return 0
+        var ws = input[0].toInt() + input[1].toInt() + input[2].toInt()
+        var count = 0
+        for(i in 3 until input.size) {
+            var nws = ws - input[i-3].toInt() + input[i].toInt()
+            if(nws > ws) {
+                count++
+            }
+        }
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
